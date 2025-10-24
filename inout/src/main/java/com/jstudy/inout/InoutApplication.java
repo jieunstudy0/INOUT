f6b41152feb9bbbe8061d048c0fb589600e19d8e
김지은 <239709374+jieunstudy0@users.noreply.github.com>
2025-10-24 17:31:13 +0900

    251024 commit test

---
+++ inout/src/main/java/com/jstudy/inout/InoutApplication.java
@@ -0,0 +1,13 @@
+package com.jstudy.inout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InoutApplication.class, args);
+	}
+
+}
---
+++ inout/src/test/java/com/jstudy/inout/InoutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jstudy.inout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InoutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
